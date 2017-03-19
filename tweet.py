9abe20eec14483165e3011a1f409dc641cfa4a71
suyooktang <suyook.tang@gmail.com>
2017-03-19 20:13:07 +0900

    점검한 문서만 트윗하도록 수정 (사용자:렌즈 님 코드 반영)

--- tweet.py
+++ tweet.py
@@ -1,10 +1,14 @@
-import os
-import re
-import random
 import json
-import urllib.parse
+import os
+import random
+import re
 import urllib
+import urllib.parse
+from inspect import getsourcefile
+
 import twitter
+
+URL_PREFIX = 'https://femiwiki.com/w/api.php?'
 
 
 def main():
@@ -14,21 +18,71 @@ def main():
         access_token_key=os.environ['TWITTER_ACCESS_TOKEN'],
         access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'],
     )
-    text = get_wikitext('페미위키:한줄인용')
+    text = get_wikitext('페미위키:한줄인용', True)
     lines = text.split('\n')
-    tweets = [line[1:].strip() for line in lines if re.match(r'^\*\s+.+$', line)]
+    tweets = [line[1:].strip() for line in lines if
+              re.match(r'^\*\s+.+$', line)]
     tweet = random.choice(tweets)
     api.PostUpdate(tweet)
 
 
-def get_wikitext(title):
-    url_prefix = 'https://femiwiki.com/w/api.php?action=query&format=json&prop=revisions&rvprop=content&titles='
-    url = url_prefix + urllib.parse.quote(title)
+def get_wikitext(title, patrolled):
+    """Returns wikitext of the document"""
+    if patrolled:
+        revid = get_patrolled_revid(title)
+        url = (
+            URL_PREFIX +
+            'action=query&format=json&prop=revisions&'
+            'rvprop=content&revids=' + str(revid)
+        )
+    else:
+        quoted_title = urllib.parse.quote(title)
+        url = (
+            URL_PREFIX +
+            'action=query&format=json&prop=revisions&'
+            'rvprop=content&titles=' + quoted_title
+        )
+
+    obj = json_from_url(url)
+    return next(iter(obj['query']['pages'].values()))['revisions'][0]['*']
+
+
+def get_patrolled_revid(title):
+    """Returns patrolled revision id of the document"""
+    revid_file = os.path.join(get_module_dir(), 'patrolled_rev_id')
+
+    try:
+        # Try to get rev id using API
+        quoted_title = urllib.parse.quote(title)
+        patrolled_logs_url = (
+            URL_PREFIX +
+            'action=query&format=json&list=logevents&leprop=details&'
+            'letype=patrol&lelimit=1&letitle=' +
+            quoted_title
+        )
+        obj = json_from_url(patrolled_logs_url)
+        revid = obj['query']['logevents'][0]['params']['curid']
+
+        # Save revid for later use
+        with open(revid_file, 'w') as f:
+            f.write(str(revid))
+    except:
+        # Try to get revid from file.
+        # (API returns nothing if recent logs don't contain patrol activity)
+        with open(revid_file, 'r') as f:
+            revid = f.readline()
+
+    return revid
+
+
+def json_from_url(url):
     with urllib.request.urlopen(url) as res:
-        obj = json.loads(res.read().decode('utf-8'))
-        return next(iter(obj['query']['pages'].values()))['revisions'][0]['*']
+        return json.loads(res.read().decode('utf-8'))
+
+
+def get_module_dir():
+    return os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
 
 
 if __name__ == '__main__':
     main()
-
