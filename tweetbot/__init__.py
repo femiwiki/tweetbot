fe186ca7b0b3a7cd711e043e83c92b25d9644cdf
lens0021 <lorentz0021@gmail.com>
2019-03-05 21:05:18 +0900

    Bug Fix

--- tweetbot/__init__.py
+++ tweetbot/__init__.py
@@ -1,9 +1,9 @@
 from .tweet import (
     main, get_wikitext, get_patrolled_revid, json_from_url, get_module_dir,
-    convert_to_tweets, choice_tweet, break_text
+    convert_to_quotations, choice_quotation, break_text
 )
 
 __all__ = [
     main, get_wikitext, get_patrolled_revid, json_from_url, get_module_dir,
-    convert_to_tweets, choice_tweet, break_text
+    convert_to_quotations, choice_quotation, break_text
 ]
